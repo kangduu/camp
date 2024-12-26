9d1f30639a6a729de0f67147a085636b315390cc
dukang <dk@nosugartech.com>
2024-12-26 17:11:07 +0800

    feat: how running ts file

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -23,5 +23,5 @@ export default sidebar({
     "/javascript/deweight.array.md",
     "/javascript/questions.md",
   ],
-  "/typescript/": ["/typescript/", "/typescript/infer.md","/typescript/satisfies-as.md"],
+  "/typescript/": ["/typescript/", "/typescript/cmd.md", "/typescript/infer.md", "/typescript/satisfies-as.md"],
 });
